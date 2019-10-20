12549323afb5a6606123174d035b5bd5a1a49c7b
Bùi Tất Trung <tattrung15@users.noreply.github.com>
2019-10-20 16:46:22 +0700

    AI tank

---
+++ copy.bat
@@ -0,0 +1,4 @@
+del "E:\Ai_tank\AI Tank Project\Arena\P1.exe"
+del "E:\Ai_tank\AI Tank Project\Arena\P2.exe"
+copy "E:\Ai_tank\AI Tank Project\Bots\C++\C++Template\2015_Debug\AI_Template.exe" "E:\Ai_tank\AI Tank Project\Arena\P1.exe"
+copy "E:\Ai_tank\AI Tank Project\Bots\C++\C++Template\2015_Debug\AI_Template.exe" "E:\Ai_tank\AI Tank Project\Arena\P2.exe"
