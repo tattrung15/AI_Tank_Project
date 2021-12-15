06b0f9c62af981dbafc8dc9fc699d6efdf4c6639
Tất Trung <55653291+tattrung15@users.noreply.github.com>
2021-12-15 11:14:50 +0700

    update copy batch file

--- copy.bat
+++ copy.bat
@@ -1,4 +1,5 @@
-del "E:\Ai_tank\AI Tank Project\Arena\P1.exe"
-del "E:\Ai_tank\AI Tank Project\Arena\P2.exe"
-copy "E:\Ai_tank\AI Tank Project\Bots\C++\C++Template\2015_Debug\AI_Template.exe" "E:\Ai_tank\AI Tank Project\Arena\P1.exe"
-copy "E:\Ai_tank\AI Tank Project\Bots\C++\C++Template\2015_Debug\AI_Template.exe" "E:\Ai_tank\AI Tank Project\Arena\P2.exe"
+del "%cd%\Arena\P1.exe"
+del "%cd%\Arena\P2.exe"
+
+copy "%cd%\Bots\C++\C++Template\2015_Debug\AI_Template.exe" "%cd%\Arena\P1.exe"
+copy "%cd%\Bots\C++\C++Template\2015_Debug\AI_Template.exe" "%cd%\Arena\P2.exe"
